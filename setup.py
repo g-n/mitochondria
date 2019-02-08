8e381eee23611292e30cdd4dbeebba800f40b1cc
g-n <g-n@users.noreply.github.com>
2019-02-07 21:51:51 -0700

    modified style formatting

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name="mitochondria",
-    python_requires=">=3.7",
+    name='mitochondria',
+    python_requires='>=3.7',
     install_requires=[
-        "Django",
-        "djangorestframework",
-        "djangorestframework_simplejwt",
-        "requests",
-        "psycopg2",
-        "gunicorn",
+        'Django',
+        'djangorestframework',
+        'requests',
+        'psycopg2',
+        'gunicorn',
     ],
 )
