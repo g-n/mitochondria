c7bb25cbca4ea5df62230b9e5af5eb7ac63f61e4
g-n <g-n@users.noreply.github.com>
2019-02-07 21:40:33 -0700

    starting over with clean project

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='mitochondria',
+    python_requires='>=3.7',
+    install_requires=[
+        'Django',
+        'djangorestframework',
+        'djangorestframework_simplejwt',
+        'requests',
+        'psycopg2',
+        'gunicorn',
+    ],
+)
+
