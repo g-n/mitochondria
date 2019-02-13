9f32d749f8ae28c3625b378250998a376d65e4d8
g-n <g-n@users.noreply.github.com>
2019-02-13 03:29:22 -0700

    changed static_root

--- mitochondria/settings.py
+++ mitochondria/settings.py
@@ -63,3 +63,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
