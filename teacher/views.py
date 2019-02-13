800a1423e95465c6c63a1245796e5b76f258ab37
g-n <g-n@users.noreply.github.com>
2019-02-13 04:29:08 -0700

    formatting/cleanup

--- teacher/views.py
+++ teacher/views.py
@@ -18,17 +18,3 @@ class GroupViewSet(viewsets.ModelViewSet):
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    """
-    List all snippets, or create a new snippet.
-    """
-    # def get(self, request, format=None):
-    #     snippets = Snippet.objects.all()
-    #     serializer = SnippetSerializer(snippets, many=True)
-    #     return Response(serializer.data)
-    #
-    # def post(self, request, format=None):
-    #     serializer = SnippetSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
